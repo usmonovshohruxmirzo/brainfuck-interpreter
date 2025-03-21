cd5ffd7c0b3d5dfc9bb14a2c6ede5602d681a20e
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-03-21 20:22:38 +0500

    feat: Precompute loop mappings

--- BrainFuck-Interpreter/Program.cs
+++ BrainFuck-Interpreter/Program.cs
@@ -5,9 +5,29 @@ namespace Program
 {
     public class Program
     {
-        byte[] memory = new byte[30000];
-        int pointer = 0;
-        Stack<int> loopStack = new Stack<int>();
-        Dictionary<int, int> loopMap = new Dictionary<int, int>();
+        public void Execute(string code)
+        {
+            byte[] memory = new byte[30_000];
+            int pointer = 0;
+            Stack<int> loopStack = new Stack<int>();
+            Dictionary<int, int> loopMap = new Dictionary<int, int>();
+
+            Stack<int> loopStartStack = new Stack<int>();
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (code[i] == '[') loopStartStack.Push(i);
+                else if (code[i] == ']')
+                {
+                    if (loopStartStack.Count == 0) throw new Exception("Unmatched ] found.");
+                    int start = loopStartStack.Pop();
+                    loopMap[start] = i;
+                    loopMap[i] = start;
+                }
+            }
+
+            if (loopStartStack.Count == 0) throw new Exception("Unmatched ] found.");
+
+
+        }
     }
 }
