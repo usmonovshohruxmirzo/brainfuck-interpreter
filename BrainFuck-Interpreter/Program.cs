e7a0454b9e5dea6903bd936f077389c4475902aa
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-03-21 20:14:30 +0500

    feat: setup vars

--- BrainFuck-Interpreter/Program.cs
+++ BrainFuck-Interpreter/Program.cs
@@ -6,6 +6,8 @@ namespace Program
     public class Program
     {
         byte[] memory = new byte[30000];
-
+        int pointer = 0;
+        Stack<int> loopStack = new Stack<int>();
+        Dictionary<int, int> loopMap = new Dictionary<int, int>();
     }
 }
